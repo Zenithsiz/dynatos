d661629ee8618b983eafa6431a57fc0b121306ee
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-02-16 16:02:26 +0000

    Added `ObjectDynProp`.

--- dynatos/src/lib.rs
+++ dynatos/src/lib.rs
@@ -13,9 +13,9 @@ pub use dyn_element::dyn_element;
 use {
 	dynatos_html::html,
 	dynatos_reactive::Effect,
-	dynatos_util::{ObjectGet, ObjectSetProp, TryOrReturnExt, WeakRef},
+	dynatos_util::{ObjectGet, ObjectRemoveProp, ObjectSetProp, TryOrReturnExt, WeakRef},
 	std::cell::RefCell,
-	wasm_bindgen::prelude::wasm_bindgen,
+	wasm_bindgen::{prelude::wasm_bindgen, JsValue},
 };
 
 /// Extension trait to add an effect to an object
@@ -395,6 +395,82 @@ where
 	}
 }
 
+/// Extension trait to add reactive prop to an object
+#[extend::ext(name = ObjectDynProp)]
+pub impl<T> T
+where
+	T: AsRef<js_sys::Object>,
+{
+	/// Adds a dynamic property to this object
+	fn dyn_prop<F, K, V>(&self, f: F)
+	where
+		F: Fn() -> (K, Option<V>) + 'static,
+		K: AsRef<str>,
+		V: Into<JsValue>,
+	{
+		// Create the value to attach
+		// Note: It's important that we only keep a `WeakRef` to the object.
+		//       Otherwise, the object will be keeping us alive, while we keep
+		//       the object alive, causing a leak.
+		let object = WeakRef::new(self.as_ref());
+		let prop_effect = Effect::try_new(move || {
+			// Try to get the object
+			let object = object.get().or_return()?;
+
+			// And set the property
+			let (key, value) = f();
+			let key = key.as_ref();
+			match value {
+				Some(value) => {
+					object.set_prop(key, value);
+				},
+				None => {
+					object.remove_prop(key);
+				},
+			}
+		})
+		.or_return()?;
+
+		// Then set it
+		self.as_ref().attach_effect(prop_effect);
+	}
+
+	/// Adds a dynamic property to this object.
+	///
+	/// Returns the object, for chaining
+	fn with_dyn_prop<F, K, V>(self, f: F) -> Self
+	where
+		F: Fn() -> (K, Option<V>) + 'static,
+		K: AsRef<str>,
+		V: Into<JsValue>,
+	{
+		self.dyn_prop(f);
+		self
+	}
+
+	/// Adds a dynamic property to this object, where only the value is dynamic.
+	fn dyn_prop_value<F, K, V>(&self, key: K, f: F)
+	where
+		F: Fn() -> Option<V> + 'static,
+		K: AsRef<str> + Copy + 'static,
+		V: Into<JsValue>,
+	{
+		self.dyn_prop(move || (key, f()));
+	}
+
+	/// Adds a dynamic property to this object, where only the value is dynamic.
+	///
+	/// Returns the object, for chaining
+	fn with_dyn_prop_value<F, K, V>(self, key: K, f: F) -> Self
+	where
+		F: Fn() -> Option<V> + 'static,
+		K: AsRef<str> + Copy + 'static,
+		V: Into<JsValue>,
+	{
+		self.dyn_prop_value(key, f);
+		self
+	}
+}
 
 /// A wasm `Effect` type.
 #[wasm_bindgen]
