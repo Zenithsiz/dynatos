4ce1529107efd00ee66536ea90a4927b2217cfd9
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-02-19 04:48:39 +0000

    Fixed `ObjectDynProp::add_dyn_prop` not behaving properly with multiple keys.

--- dynatos/src/object_dyn_prop.rs
+++ dynatos/src/object_dyn_prop.rs
@@ -5,6 +5,7 @@ use {
 	crate::ObjectAttachEffect,
 	dynatos_reactive::Effect,
 	dynatos_util::{ObjectRemoveProp, ObjectSetProp, TryOrReturnExt, WeakRef},
+	std::cell::RefCell,
 	wasm_bindgen::JsValue,
 };
 
@@ -18,18 +19,38 @@ pub impl js_sys::Object {
 		K: AsRef<str>,
 		V: Into<JsValue>,
 	{
-		// Create the value to attach
+		// The object we're attaching to
 		// Note: It's important that we only keep a `WeakRef` to the object.
 		//       Otherwise, the object will be keeping us alive, while we keep
 		//       the object alive, causing a leak.
 		let object = WeakRef::new(self);
+
+		// The last set key value.
+		// Note: We don't actually store the previous value, since we don't
+		//       have a need for it. Instead we just store if there was a value.
+		let last_key_value = RefCell::new(None::<(String, bool)>);
+
+		// Create the effect
 		let prop_effect = Effect::try_new(move || {
 			// Try to get the object
 			let object = object.get().or_return()?;
 
-			// And set the property
+			// Then get the property
 			let (key, value) = f();
 			let key = key.as_ref();
+
+			// If we've already set a property with a value, and the current
+			// key is different, remove the old one first.
+			if let Some((ref last_key, has_value)) = *last_key_value.borrow() &&
+				has_value && last_key != key
+			{
+				object.remove_prop(last_key);
+			}
+
+			// Then update the last key-value
+			*last_key_value.borrow_mut() = Some((key.to_owned(), value.is_some()));
+
+			// And finally set/remove the property
 			match value {
 				Some(value) => {
 					object.set_prop(key, value);
