12b88120ef0e830f206e041f42fd0a9dfa37ccf3
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-02-27 11:40:00 +0000

    Added some documentation to `dynatos_reactive::Derived`.

--- dynatos-reactive/src/derived.rs
+++ dynatos-reactive/src/derived.rs
@@ -1,4 +1,34 @@
-//! Derived signal
+//! # Derived signals
+//!
+//! A derived signal, [`Derived`], is a signal that caches a reactive function's result, that is,
+//! a function that depends on other signals.
+//!
+//! This is useful for splitting up an effect that requires computing multiple expensive operations,
+//! to avoid needless re-computing certain values when others change.
+//!
+//! ## Examples
+//! Without using a derived, whenever any dependent signals of `expensive_operation1` or
+//! `expensive_operation2` are updated, then they will both be re-run due to `my_value`
+//! requiring an update.
+//! ```rust,no_run
+//! // Pretend these are expensive operations
+//! let expensive_operation1 = move || 1;
+//! let expensive_operation2 = move || 2;
+//! let my_value = move || expensive_operation1() + expensive_operation2();
+//! ```
+//!
+//! Meanwhile, when using [`Derived`], you can cache each value, so that any updates
+//! to one of the signals doesn't re-compute the other.
+//! ```rust,no_run
+//! use dynatos_reactive::{Derived, SignalGet};
+//! let expensive_operation1 = Derived::new(move || 1);
+//! let expensive_operation2 = Derived::new(move || 2);
+//! let my_value = move || expensive_operation1.get() + expensive_operation2.get();
+//! ```
+//!
+//! It's important to note that this isn't free however, as [`Derived`] needs to
+//! not only store the latest value, it also needs to create an effect that is re-run
+//! each time any dependencies are updated.
 
 // TODO: Make `Derived` always `usize`-sized
 //       by merging the `effect::Inner` and `signal::Inner` somehow?
@@ -9,7 +39,9 @@ use {
 	std::fmt,
 };
 
-/// Derived signal
+/// Derived signal.
+///
+/// See the module documentation for more information.
 pub struct Derived<T> {
 	/// Effect
 	effect: Effect,
