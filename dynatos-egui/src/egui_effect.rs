7398a7e34cadae9239fe46a678ce4b6f5afdc55b
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2024-12-28 07:11:42 +0000

    Introduced experimental support for egui.

---
+++ dynatos-egui/src/egui_effect.rs
@@ -0,0 +1,39 @@
+//! Egui effect
+
+// Imports
+use dynatos_reactive::{effect::EffectDepsGatherer, Effect};
+
+/// An effect that redraws an egui context whenever any signals change
+pub struct EguiEffect {
+	/// Effect
+	effect: Effect<EffectFn>,
+}
+
+impl EguiEffect {
+	/// Creates a new egui effect from an egui context
+	#[must_use]
+	pub fn new(ctx: egui::Context) -> Self {
+		Self {
+			effect: Effect::new_raw(effect_fn(ctx)),
+		}
+	}
+
+	/// Returns the effect dependency gatherer.
+	///
+	/// You should call this within `draw`, and keep it around until you finish drawing.
+	#[must_use]
+	pub fn deps_gatherer(&self) -> EffectDepsGatherer {
+		self.effect.deps_gatherer()
+	}
+}
+
+use effect_fn::*;
+mod effect_fn {
+	pub type EffectFn = impl Fn();
+	pub fn effect_fn(ctx: egui::Context) -> EffectFn {
+		move || {
+			tracing::debug!("Request redraw");
+			ctx.request_repaint();
+		}
+	}
+}
---
+++ dynatos-egui/src/lib.rs
@@ -0,0 +1,10 @@
+//! `dynatos`'s `egui` support.
+
+// Features
+#![feature(type_alias_impl_trait)]
+
+// Modules
+mod egui_effect;
+
+// Exports
+pub use egui_effect::EguiEffect;
