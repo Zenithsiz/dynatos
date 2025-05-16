42b96c4c9dc65f0be0daf2f2001972411e6d3e51
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2025-05-16 17:33:31 +0100

    `dynatos_router` now exports it's signals' modules.

--- dynatos-router/src/lib.rs
+++ dynatos-router/src/lib.rs
@@ -5,9 +5,9 @@
 
 // Modules
 mod anchor;
-mod location;
-mod query_array_signal;
-mod query_signal;
+pub mod location;
+pub mod query_array_signal;
+pub mod query_signal;
 
 // Exports
 pub use self::{anchor::anchor, location::Location, query_array_signal::QueryArraySignal, query_signal::QuerySignal};
