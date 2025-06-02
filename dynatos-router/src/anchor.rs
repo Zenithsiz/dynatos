73550489c532098cdcd80fb1ea7b8c67cd2199dd
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2025-06-02 02:56:14 +0100

    Fixed warning when using an anchor outside of an effect.

--- dynatos-router/src/anchor.rs
+++ dynatos-router/src/anchor.rs
@@ -21,7 +21,7 @@ where
 			ev.prevent_default();
 			dynatos_context::with_expect::<Location, _, _>(|location| {
 				let new_location = new_location.as_ref();
-				let res = location.borrow().join(new_location);
+				let res = location.borrow_raw().join(new_location);
 				match res {
 					Ok(new_location) => location.set(new_location),
 					Err(err) => tracing::warn!("Unable to join new location to current {new_location:?}: {err}"),
